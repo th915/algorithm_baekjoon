20543d9aa28d69f7e5ab5cd8f1f506c5e8424f39
th915 <97163658+th915@users.noreply.github.com>
2024-02-03 01:34:41 +0900

    Add files via upload

---
+++ Silver/1316번.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < num; i++) {
+            String word = sc.next();
+
+            if(word.length() == 1){
+                count++;
+                continue;
+            }
+
+            for (int j = 0; j < word.length()-1; j++) {
+
+                if(word.charAt(j) != word.charAt(j+1) && word.substring(j+1).indexOf(word.charAt(j)) != -1){
+                    break;
+                }
+
+                if(j == word.length()-2){
+                    count++;
+                }
+            }   
+        }
+        System.out.println(count);
+    }
+}
+
