fb0e0b1ec2d5045fb934a909acc2a60788f64b57
th915 <97163658+th915@users.noreply.github.com>
2024-02-05 20:27:40 +0900

    1244 commit

---
+++ Silver/main_1244.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class main_1244 {
+
+    public static int[] arrays;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        arrays = new int[num+1];
+        for (int i = 1; i <= num; i++) {
+            arrays[i] = sc.nextInt();
+        }
+
+        int person = sc.nextInt();
+        for (int i = 0; i < person; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if(y == num && (x == 2 && y == 1)){
+                change(y);
+            } else if (x == 1){
+                for (int j = y; j < arrays.length; j += y) {
+                    change(j);
+                }
+            } else {
+                int j = 1;
+                change(y);
+                while(y+j < arrays.length && y-j > 0){
+                    if(arrays[y+j] != arrays[y-j]){
+                        break;
+                    }
+                    change(y+j);
+                    change(y-j);
+                    j++;
+                }
+            }
+        }
+
+        for (int i = 1; i < arrays.length-1; i++) {
+            System.out.print(arrays[i] + " ");
+            if(i % 20 == 0){
+                System.out.print("\n");
+            }
+        }
+        
+    }
+
+    public static void change(int a){
+        arrays[a] = (arrays[a] == 0)? 1 : 0;
+    }
+}
